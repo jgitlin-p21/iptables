01831de6b277b5a3c943c49958e4543869e243ee
Josh Gitlin <jgitlin@pinnacle21.com>
2021-05-05 15:16:03 -0400

    P21: Update metadata for forked version

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,9 @@ maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
 license           'Apache-2.0'
 description       'Installs the iptables daemon and provides resources for managing rules'
-source_url        'https://github.com/chef-cookbooks/iptables'
-issues_url        'https://github.com/chef-cookbooks/iptables/issues'
-chef_version      '>= 14'
+source_url        'https://github.com/jgitlin-p21/iptables'
+issues_url        'https://github.com/jgitlin-p21/iptables/issues'
+chef_version      '>= 16'
 version           '21.7.2'
 
 %w(redhat centos debian ubuntu amazon scientific oracle amazon zlinux).each do |os|
